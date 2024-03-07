988bf9e6a3d3f1cd0463a6cf59e4fb2a532c8f5f
ig <ig@igs-MacBook-Pro.lan>
2024-03-07 08:07:05 -0800

    Added version 2 with slider and text input

--- src/App.js
+++ src/App.js
@@ -13,20 +13,28 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
-  const date = new Date("march 5 2024");
+  const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   return (
     <div>
       <div>
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
       </div>
 
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
 
@@ -36,7 +44,7 @@ function Counter() {
             ? "Today is "
             : count > 0
             ? `${count} days from today is `
-            : `${Math.abs(count)} days ago was. `}
+            : `${Math.abs(count)} days ago was `}
         </span>
         <span>{date.toDateString()}</span>
       </p>
